f29b53b131d330106d95c07d66cc6f7861e2c1c1
MCavieres <maca.c.91@gmail.com>
2024-04-15 13:28:57 -0400

    se agrega medias queries, input fecha limite y realizacion de js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 const contenidoTareas = document.querySelector("#contenidoTareas");
 const formulario = document.querySelector("#formulario");
-const tanda = document.querySelector(".tanda");
 const alerta = document.querySelector(".alerta");
 const template = document.querySelector("#template").content;
 const fragment = document.createDocumentFragment();
@@ -11,12 +10,20 @@ formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     agregarTarea();
     pintarTarea();
+
+    tarea.value = "";
 });
 
 const agregarTarea = () => {
     const tarea = document.querySelector("#tarea").value;
+    const fechaInput = document.querySelector("#limite").value;
+
+    const termino = new Date(fechaInput).toLocaleString();
+
+    console.log(termino);
 
     if (!tarea.trim()) {
+        alerta.style.color = "red";
         alerta.textContent = "Agregue una tarea";
         return;
     } else {
@@ -27,20 +34,33 @@ const agregarTarea = () => {
         id: Date.now(),
         task: tarea,
         fecha: new Date().toLocaleString(),
+        fechaTermino: termino,
     };
 
     arrayTareas.push(quehacer);
-
-    console.log(arrayTareas);
 };
 
 const pintarTarea = () => {
+    contenidoTareas.textContent = "";
+
     arrayTareas.forEach((item) => {
         const clone = template.cloneNode(true);
         clone.querySelector(".tanda").textContent = item.task;
+        clone.querySelector(".fecha").textContent = item.fecha;
+        clone.querySelector(".termino").textContent = item.fechaTermino;
+        clone.querySelector("article").classList.add(`${item.id}`);
 
         fragment.appendChild(clone);
     });
 
     contenidoTareas.appendChild(fragment);
 };
+
+contenidoTareas.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btnEliminar")) {
+        const articleClass = +e.target.parentNode.parentNode.className;
+        const article = arrayTareas.findIndex((item) => item.id === articleClass);
+        arrayTareas.splice(article, 1);
+        pintarTarea();
+    }
+});
